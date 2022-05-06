366f707eed8986b607fbd20d5e38d6aa9c550889
HoseinBaharlou <hossinbahar98@gmail.com>
2022-05-06 16:23:28 +0400

    create calendar

---
+++ pages/dashboard/calendar.vue
@@ -0,0 +1,17 @@
+<template>
+    <!-- start calendar -->
+    <v-card class="rounded-xl pa-5" elevation="6">
+        <v-card-actions>
+            <calendar/>
+        </v-card-actions>
+    </v-card>
+    <!-- end calendar -->
+</template>
+
+<script>
+import calendar from '../../components/partials/dashboard/Calendar.vue'
+export default {
+    layout:'dashboard',
+    components:{calendar}
+}
+</script>
