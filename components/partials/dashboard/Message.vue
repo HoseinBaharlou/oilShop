5328313df50655e422188e7557a48dfda2b94f99
HoseinBaharlou <hossinbahar98@gmail.com>
2022-05-06 16:06:18 +0400

    create component for Message page

---
+++ components/partials/dashboard/Message.vue
@@ -0,0 +1,118 @@
+<template>
+    <v-card class="rounded-l-xl pb-5" elevation="10">
+        <v-card-actions class="d-block">
+          <v-row class="px-5">
+            <!-- start user text -->
+            <v-col cols="12">
+              <div>
+                <v-avatar>
+                  <v-img src="../icons/Oval.svg" lazy-src="../icons/Oval.svg"/>
+                </v-avatar>
+                <span class="mx-3">پشه</span>
+                <small class="grey--text">12:10</small>
+              </div>
+              <div class="blue lighten-5 rounded-lg py-2 mt-2 w-80">
+                <span class="pr-5">!بده بزنیم</span>
+              </div>
+            </v-col>
+            <!-- end user text -->
+
+            <!-- start admin text -->
+            <v-col cols="12" class="d-flex justify-end">
+              <section class="w-70">
+                <div class="d-flex justify-end align-center">
+                  <v-avatar class="order-3">
+                    <v-img src="../icons/Profile_image.svg" lazy-src="../icons/Profile_image.svg"/>
+                  </v-avatar>
+                  <span class="px-3 order-2">آقای مجری</span>
+                  <small class="grey--text order-1">12:10</small>
+                </div>
+                <div class="blue lighten-5 rounded-lg py-2 mt-2">
+                  <span class="pr-5">!بده بزنیم</span>
+                </div>
+              </section>
+            </v-col>
+            <!-- end admin text -->
+
+            <!-- start user text -->
+            <v-col cols="12">
+              <div>
+                <v-avatar>
+                  <v-img src="../icons/Oval.svg" lazy-src="../icons/Oval.svg"/>
+                </v-avatar>
+                <span class="mx-3">پشه</span>
+                <small class="grey--text">12:10</small>
+              </div>
+              <div class="blue lighten-5 rounded-lg py-2 mt-2 w-80">
+                <span class="pr-5">!بده بزنیم</span>
+              </div>
+            </v-col>
+            <!-- end user text -->
+
+            <!-- start admin text -->
+            <v-col cols="12" class="d-flex justify-end">
+              <section class="w-70">
+                <div class="d-flex justify-end align-center">
+                  <v-avatar class="order-3">
+                    <v-img src="../icons/Profile_image.svg" lazy-src="../icons/Profile_image.svg"/>
+                  </v-avatar>
+                  <span class="px-3 order-2">آقای مجری</span>
+                  <small class="grey--text order-1">12:10</small>
+                </div>
+                <div class="blue lighten-5 rounded-lg py-2 mt-2">
+                  <span class="pr-5">!بده بزنیم</span>
+                </div>
+              </section>
+            </v-col>
+            <!-- end admin text -->
+
+            <!--start not read line -->
+            <v-col cols="12" class="d-flex align-center">
+              <span>نخوانده</span>
+              <v-divider></v-divider>
+            </v-col>
+            <!--start not read line -->
+
+            <!-- start not read message -->
+            <v-col cols="12">
+              <div>
+                <v-avatar>
+                  <v-img src="../icons/Oval.svg" lazy-src="../icons/Oval.svg"/>
+                </v-avatar>
+                <span class="mx-3">پشه</span>
+                <small class="grey--text">12:10</small>
+              </div>
+              <div class="mt-4">
+                <span class="blue rounded-lg py-2 mt-5 px-7">!بده بزنیم</span>
+              </div>
+            </v-col>
+            <!-- end not read message -->
+            <v-col cols="12" class="custom-input-send-message">
+              <v-row>
+                <v-col cols="10" class="d-flex justify-end px-0">
+                  <v-text-field outlined filled placeholder="اینجا تایپ کن..." class="rounded-xl-l"></v-text-field>
+                </v-col>
+                <v-col cols="2" class="d-flex justify-start px-0">
+                  <v-btn class="blue white--text rounded-l-xl" x-large height="56" elevation="0">
+                  ارسال
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<style lang='scss'>
+.custom-input-send-message{
+  .v-btn{
+    border-top-right-radius: unset !important;
+    border-bottom-right-radius:unset !important ;
+  }
+  .v-input__control{
+    border-top-left-radius: unset !important;
+    border-bottom-left-radius:unset !important ;
+  }
+}
+</style>
--- pages/dashboard/Message.vue
+++ pages/dashboard/Message.vue
@@ -2,120 +2,25 @@
   <v-row>
     <!-- start contacts -->
     <v-col md="4">
-      <v-navigation-drawer class="rounded-r-xl elevation-10" v-model="drawer">
-        <div  class="px-5">
-          <v-text-field label="پیدا کردن مخاطبین"><v-icon slot="append" color="grey" label="پیدا کردن مخاطبین">mdi-magnify</v-icon></v-text-field>
-        </div>
-        <v-list-item-group v-model="model">
-          <contacts/>
-        </v-list-item-group>
-      </v-navigation-drawer>
+      <Contacts/>
     </v-col>
     <!-- end contacts -->
     <!-- start message -->
     <v-col md="8">
-      <v-card class="rounded-l-xl pb-5" elevation="10">
-        <v-card-actions class="d-block">
-          <v-row class="px-5">
-            <!-- start user text -->
-            <v-col cols="12">
-              <div>
-                <v-avatar>
-                  <v-img src="../icons/Oval.svg" lazy-src="../icons/Oval.svg"/>
-                </v-avatar>
-                <span class="mx-3">پشه</span>
-                <small class="grey--text">12:10</small>
-              </div>
-              <div class="blue lighten-5 rounded-lg py-2 mt-2 w-80">
-                <span class="pr-5">!بده بزنیم</span>
-              </div>
-            </v-col>
-            <!-- end user text -->
-
-            <!-- start admin text -->
-            <v-col cols="12" class="d-flex justify-end">
-              <section class="w-70">
-                <div class="d-flex justify-end align-center">
-                  <v-avatar class="order-3">
-                    <v-img src="../icons/Profile_image.svg" lazy-src="../icons/Profile_image.svg"/>
-                  </v-avatar>
-                  <span class="px-3 order-2">آقای مجری</span>
-                  <small class="grey--text order-1">12:10</small>
-                </div>
-                <div class="blue lighten-5 rounded-lg py-2 mt-2">
-                  <span class="pr-5">!بده بزنیم</span>
-                </div>
-              </section>
-            </v-col>
-            <!-- end admin text -->
-
-            <!-- start user text -->
-            <v-col cols="12">
-              <div>
-                <v-avatar>
-                  <v-img src="../icons/Oval.svg" lazy-src="../icons/Oval.svg"/>
-                </v-avatar>
-                <span class="mx-3">پشه</span>
-                <small class="grey--text">12:10</small>
-              </div>
-              <div class="blue lighten-5 rounded-lg py-2 mt-2 w-80">
-                <span class="pr-5">!بده بزنیم</span>
-              </div>
-            </v-col>
-            <!-- end user text -->
-
-            <!-- start admin text -->
-            <v-col cols="12" class="d-flex justify-end">
-              <section class="w-70">
-                <div class="d-flex justify-end align-center">
-                  <v-avatar class="order-3">
-                    <v-img src="../icons/Profile_image.svg" lazy-src="../icons/Profile_image.svg"/>
-                  </v-avatar>
-                  <span class="px-3 order-2">آقای مجری</span>
-                  <small class="grey--text order-1">12:10</small>
-                </div>
-                <div class="blue lighten-5 rounded-lg py-2 mt-2">
-                  <span class="pr-5">!بده بزنیم</span>
-                </div>
-              </section>
-            </v-col>
-            <!-- end admin text -->
-
-            <!--start not read line -->
-            <v-col cols="12" class="d-flex align-center">
-              <span>نخوانده</span>
-              <v-divider></v-divider>
-            </v-col>
-            <!--start not read line -->
-
-            <!-- start not read message -->
-            <v-col cols="12">
-              <div>
-                <v-avatar>
-                  <v-img src="../icons/Oval.svg" lazy-src="../icons/Oval.svg"/>
-                </v-avatar>
-                <span class="mx-3">پشه</span>
-                <small class="grey--text">12:10</small>
-              </div>
-              <div class="mt-4">
-                <span class="blue rounded-lg py-2 mt-5 px-7">!بده بزنیم</span>
-              </div>
-            </v-col>
-            <!-- end not read message -->
-          </v-row>
-        </v-card-actions>
-      </v-card>
+      <Message/>
     </v-col>
     <!-- end message -->
   </v-row>
 </template>
 
 <script>
-import Contacts from '../../components/partials/dashboard/Contacts.vue'
+import Contacts from '../../components/partials/dashboard/contacts.vue'
+import Message from '../../components/partials/dashboard/Message.vue'
 export default {
   setup() {
     
   },
+  components:{Contacts,Message},
   layout:'dashboard',
   data:()=>{
     return{
@@ -124,6 +29,6 @@ export default {
       mini:false
     }
   },
-  components:{Contacts}
 }
 </script>
+
