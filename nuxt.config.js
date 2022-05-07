d4ee34f95bb27fed3c6544ae3119ebcbcf072056
HoseinBaharlou <hossinbahar98@gmail.com>
2022-05-07 22:21:31 +0400

    create analyse page and finish and add dropzone package

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,8 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     {src : '~/plugins/vue-apexchart.js', ssr : false},
-    {src : '~/plugins/vue2-google-maps.js', ssr : false}
+    {src : '~/plugins/vue2-google-maps.js', ssr : false},
+    {src : '~/plugins/dropzone.js', ssr : false}
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
---
+++ plugins/dropzone.js
@@ -0,0 +1,6 @@
+import vue2Dropzone from 'vue2-dropzone'
+import Vue from 'vue'
+
+import 'vue2-dropzone/dist/vue2Dropzone.min.css'
+
+Vue.component('vue2Dropzone', vue2Dropzone)
