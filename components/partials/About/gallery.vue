b69be0a92e110e3a95da727e7dab05a8787fa599
HoseinBaharlou <hossinbahar98@gmail.com>
2022-05-07 21:04:26 +0400

    create about page and finish

---
+++ components/partials/About/gallery.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-row>
+        <v-col cols="12" md="4">
+            <v-img src="image/Rectangle 240.png" lazy-src="image/Rectangle 240.png" />
+        </v-col>
+
+        <v-col cols="12" md="4">
+            <v-img src="image/Rectangle 240.png" lazy-src="image/Rectangle 240.png" />
+        </v-col>
+
+        <v-col cols="12" md="4">
+            <v-img src="image/Rectangle 242.png" lazy-src="image/Rectangle 242.png" />
+        </v-col>
+    </v-row>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,55 @@
+<template>
+    <v-container fluid>
+        <v-row class="article">
+            <v-col cols="10" offset="1">
+                <v-row>
+                    <!-- start breadcrumbs -->
+                    <v-col cols="12">
+                        <v-breadcrumbs :items="items"></v-breadcrumbs>
+                        <v-divider></v-divider>
+                    </v-col>
+                    <!-- end breadcrumbs -->
+
+                    <!--start information -->
+                    <v-col cols="12" class="mt-7" > 
+                        <about/>
+                    </v-col>
+                    <!-- end information -->
+
+                    <!-- start gallery -->
+                    <v-col cols="12" class="mt-24">
+                        <gallery/>
+                    </v-col>
+                    <!-- end gallery -->
+                </v-row>   
+            </v-col> 
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+import about from '../components/partials/About/about.vue'
+import gallery from '../components/partials/About/gallery.vue'
+export default {
+    setup() {
+        
+    },
+    components:{about,gallery},
+    data:()=>{
+        return{
+            items: [
+                {
+                    text: 'صفحه اصلی',
+                    disabled: true,
+                    href: 'breadcrumbs_dashboard',
+                },
+                {
+                    text: 'درباره ما',
+                    disabled: true,
+                    href: 'breadcrumbs_dashboard',
+                },
+            ],
+        }
+    }
+}
+</script>
